3dfa6e825ed855e82ab004165145bddf9b3a24b8
oleg <7vb3msb@mail.ru>
2024-11-05 13:23:56 +0300

    Create model Apartment and apply migrations

--- real_home/realty/models.py
+++ real_home/realty/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Apartment(models.Model):
+    on_floor = models.IntegerField(verbose_name='Количество квартир на этаже')
+    rooms = models.IntegerField(verbose_name='Количество комнат')
+    total_area = models.IntegerField(verbose_name='Общая площадь')
+    living_area = models.IntegerField(verbose_name='Жилая площадь')
+    kitchen_area = models.IntegerField(verbose_name='Площадь кухни')
+    is_balcony = models.BooleanField(verbose_name='Наличие балкона')
+    price_square = models.IntegerField(verbose_name='Цена за кв.м.')
+    price_flat = models.IntegerField(verbose_name='Цена за квартиру')
