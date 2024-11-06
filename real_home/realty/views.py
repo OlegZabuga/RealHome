a6bd062b8aafd49881bd3814803b98b4042f5541
oleg <7vb3msb@mail.ru>
2024-11-06 11:40:19 +0300

    Add global pagination Add pagination for Appartment Change path for app realty (.../api/...)

--- real_home/realty/views.py
+++ real_home/realty/views.py
@@ -1,8 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, pagination
 from .models import Apartment
 from .serializers import ApartmentSerializer
+
+
+class ApartmentPagination(pagination.PageNumberPagination):
+    page_size = 5
 
 
 class ApartmentList(generics.ListAPIView):
     queryset = Apartment.objects.all()
     serializer_class = ApartmentSerializer
+    pagination_class = ApartmentPagination
