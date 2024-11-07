75c85d17822c1ebfbf16dd8456cf4d628c04f06c
oleg <7vb3msb@mail.ru>
2024-11-07 13:11:59 +0300

    Create models Section and ApartmentType Fix any models

--- real_home/realty/models.py
+++ real_home/realty/models.py
@@ -2,7 +2,18 @@ from django.db import models
 
 
 class Apartment(models.Model):
-    on_floor = models.IntegerField(verbose_name='Количество квартир на этаже')
+    num = models.IntegerField(null=True, verbose_name='Номер квартиры')
+    on_sale = models.BooleanField(verbose_name='В продаже', null=True)
+    type = models.ForeignKey('ApartmentType', null=True, on_delete=models.PROTECT, verbose_name='Тип квартиры')
+    floor = models.ForeignKey('Floor', on_delete=models.PROTECT, null=True, verbose_name='Этаж')
+    section = models.ForeignKey('Section', on_delete=models.PROTECT, null=True, verbose_name='Секция')
+
+    def __str__(self):
+        return f'Квартира №{self.num} на этаже {self.floor}'
+
+
+class ApartmentType(models.Model):
+    plane = models.CharField(max_length=100, verbose_name='Планировка', null=True)
     rooms = models.IntegerField(verbose_name='Количество комнат')
     total_area = models.IntegerField(verbose_name='Общая площадь')
     living_area = models.IntegerField(verbose_name='Жилая площадь')
@@ -10,14 +21,21 @@ class Apartment(models.Model):
     is_balcony = models.BooleanField(verbose_name='Наличие балкона')
     price_square = models.IntegerField(verbose_name='Цена за кв.м.')
     price_flat = models.IntegerField(verbose_name='Цена за квартиру')
-    on_sale = models.BooleanField(default=True, verbose_name='В продаже')
-    floor = models.ForeignKey('Floor', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
-        return f'Квартира №{self.id} на этаже {self.floor}'
+        return self.plane
 
 
 class Floor(models.Model):
+    amount_flats = models.IntegerField(null=True, blank=True, verbose_name='Количество квартир на этаже')
+    section = models.ManyToManyField('Section', related_name='floors', verbose_name='Секция')
 
     def __str__(self):
         return f'Этаж {self.id}'
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=10, verbose_name='Секция')
+
+    def __str__(self):
+        return f'Секция {self.name}'
