e51c7caf0d0d4de796eea58c44e03988ff9de02a
oleg <7vb3msb@mail.ru>
2024-11-07 14:02:05 +0300

    Create migrations Add data in db Fix API for Apartment

--- real_home/realty/serializers.py
+++ real_home/realty/serializers.py
@@ -1,15 +1,23 @@
 from rest_framework import serializers
 from rest_framework.relations import PrimaryKeyRelatedField
+from .models import Apartment, Floor, Section, ApartmentType
 
-from .models import Apartment, Floor
+
+class ApartmentTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApartmentType
+        fields = '__all__'
 
 
 class ApartmentSerializer(serializers.ModelSerializer):
+    type = ApartmentTypeSerializer()
     floor = PrimaryKeyRelatedField(queryset=Floor.objects.all())
+    section = PrimaryKeyRelatedField(queryset=Section.objects.all())
 
     class Meta:
         model = Apartment
-        fields = '__all__'
+        fields = ['num', 'on_sale', 'type', 'floor', 'section']
+
 
 
 class FloorSerializer(serializers.ModelSerializer):
