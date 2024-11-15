b22b062278382b88066f546dec2e806a96865171
oleg <7vb3msb@mail.ru>
2024-11-15 14:32:56 +0300

    Implement easy Repository Pattern

---
+++ real_home/realty/repository.py
@@ -0,0 +1,26 @@
+from .models import Apartment, Floor, Building, Project
+
+
+class ApartmentRepository:
+    @staticmethod
+    def get_all():
+        return Apartment.objects.all()
+
+
+
+class FloorRepository:
+    @staticmethod
+    def get_all():
+        return Floor.objects.all()
+
+
+class BuildingRepository:
+    @staticmethod
+    def get_all():
+        return Building.objects.all()
+
+
+class ProjectRepository:
+    @staticmethod
+    def get_all():
+        return Project.objects.all()
--- real_home/realty/views.py
+++ real_home/realty/views.py
@@ -1,26 +1,26 @@
 from rest_framework import generics
-from .models import Apartment, Floor, Building, Project
 from .serializers import ApartmentSerializer, FloorSerializer, BuildingSerializer, ProjectSerializer
-from.pagination import ApartmentPagination, FloorPagination
+from .pagination import ApartmentPagination, FloorPagination
+from .repository import ApartmentRepository, FloorRepository, BuildingRepository, ProjectRepository
 
 
 class ApartmentListAPI(generics.ListCreateAPIView):
-    queryset = Apartment.objects.all()
+    queryset = ApartmentRepository.get_all()
     serializer_class = ApartmentSerializer
     pagination_class = ApartmentPagination
 
 
 class FloorListAPI(generics.ListCreateAPIView):
-    queryset = Floor.objects.all()
+    queryset = FloorRepository.get_all()
     serializer_class = FloorSerializer
     pagination_class = FloorPagination
 
 
 class BuildingListAPI(generics.ListCreateAPIView):
-    queryset = Building.objects.all()
+    queryset = BuildingRepository.get_all()
     serializer_class = BuildingSerializer
 
 
 class ProjectListAPI(generics.ListCreateAPIView):
-    queryset = Project.objects.all()
+    queryset = ProjectRepository.get_all()
     serializer_class = ProjectSerializer
