c8ddac8165c44fb5a8819eab06b4ad2af05aed1c
oleg <7vb3msb@mail.ru>
2024-11-20 12:34:16 +0300

    Remove forms and fix admin.py

--- real_home/realty/admin.py
+++ real_home/realty/admin.py
@@ -1,39 +1,68 @@
 from django.contrib import admin
 from .models import Apartment, ApartmentType, Floor, Section, Building, Project
-from .forms import ApartmentForm, ApartmentTypeForm, FloorForm, SectionForm, BuildingForm, ProjectForm
 
 
 @admin.register(Apartment)
 class ApartmentAdmin(admin.ModelAdmin):
-    form = ApartmentForm
-    list_display = ('num', 'building', 'image_tag')
+    list_display = (
+        'num',
+        'on_sale',
+        'building',
+        'image_tag'
+    )
+    search_fields = ('num',)
+    list_editable = ('on_sale',)
     readonly_fields = ('image_tag',)
+    list_filter = ('type', 'on_sale',)
 
 
 @admin.register(ApartmentType)
 class ApartmentTypeAdmin(admin.ModelAdmin):
-    form = ApartmentTypeForm
-
+    list_display = (
+        'plane',
+        'rooms',
+    )
+    list_filter = ('rooms',)
+    search_fields = ('rooms',)
 
 @admin.register(Floor)
 class FloorAdmin(admin.ModelAdmin):
-    form = FloorForm
-
+    list_display = (
+        'floor_number',
+        'section',
+    )
+    search_fields = ('floor_number',)
 
 @admin.register(Section)
 class SectionAdmin(admin.ModelAdmin):
-    form = SectionForm
+    list_display = (
+        'name',
+        'building'
+    )
+    search_fields = ('building',)
 
 
 @admin.register(Building)
 class BuildingAdmin(admin.ModelAdmin):
-    form = BuildingForm
-    list_display = ('num', 'on_street', 'project', 'image_tag')
+    list_display = (
+        'num',
+        'on_street',
+        'project',
+        'image_tag'
+    )
     readonly_fields = ('image_tag',)
+    search_fields = ('num',)
+    list_filter = ('project',)
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    form = ProjectForm
-    list_display = ('name', 'amount_floors', 'rating', 'image_tag')
+    list_display = (
+        'name',
+        'amount_floors',
+        'rating',
+        'image_tag'
+    )
+    search_fields = ('rating', 'amount_floors')
     readonly_fields = ('image_tag',)
+    list_filter = ('rating', 'amount_floors')
